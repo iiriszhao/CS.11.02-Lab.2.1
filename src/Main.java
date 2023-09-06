f5c97d01c9a6ba45d25a1c18ba905566c4b92e2b
iiriszhao <143477473+iiriszhao@users.noreply.github.com>
2023-09-06 11:33:41 +0800

    YAY

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,36 @@
 public class Main {
 
-    public static void main(String[] args) {
-
+    public static int add(int a, int b) {
+        return a + b;
     }
 
+    public static int add(int a, int b, int c, int d) {
+        return a + b + c + d;
+    }
+
+    public static String afternoonGreeting(String name) {
+        return "下午好, " + name + "!";
+    }
+
+    public static String morningGreeting (String name) {
+        return "早上好, " + name + "!";
+    }
+
+    public static String triple(String str) {
+        return str + str + str;
+    }
+
+    public static double half(double num) {
+        return num / 2;
+    }
+
+    public static int roundPositiveValueToNearestInteger(double posdbl) {
+        return (int) (posdbl + 0.5);
+    }
+
+    public static int roundNegativeValueToNearestInteger(double negdbl) {
+        return (int) (negdbl - 0.5);
+    }
     // 1. add
     
     // 2. add
